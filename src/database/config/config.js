608f6dfe5cb2301fbc41ab16fad12844398d5fd2
Chinedu Orie <nedsoftdeveloper@gmail.com>
2021-07-11 00:04:27 +0100

    refactor: deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,7 +4,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-
 module.exports = {
   development: {
     username: process.env.DB_USER || 'root',
@@ -12,6 +11,12 @@ module.exports = {
     database: process.env.DB_NAME || 'cake_dev_db',
     host: process.env.DB_HOST || 'localhost',
     dialect: process.env.DB_DIALECT || 'sqlite',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    },
   },
   test: {
     username: 'root',
@@ -22,10 +27,16 @@ module.exports = {
     storage: '.test_db',
   },
   production: {
-    username: process.env.DB_USER ||  'root',
+    username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'cake_prod_db',
     host: process.env.DB_HOST || 'localhost',
     dialect: process.env.DB_DIALECT || 'sqlite',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    },
   },
 };
