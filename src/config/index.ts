c5f515a2015d4495c4038855334accd777aa3c50
Chinedu Orie <nedsoftdeveloper@gmail.com>
2022-01-31 18:20:52 +0000

    Revert "feat: implement deal endpoints"
    This reverts commit 023790d4b48559426995f7f175922aa82c6d5e0b.

--- src/config/index.ts
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-export const byBit = {
-  apiKey: process.env.BYBIT_API_KEY || '',
-  baseURL: process.env.BYBIT_BASE_URL || '',
-  secret: process.env.BYBIT_SECRET || ''
-};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,25 +1,20 @@
 import { Router } from 'express';
-import OrderController from '../controllers/OrderController';
+import CakeController from '../controllers/CakeController';
+import { createCakeSchema } from '../validations/schemas/cake';
+import Validator from '../validations/Validator';
 const router = Router();
 
-const {
-  createOrder,
-  getActiveOrder,
-  getPosition,
-  setLeverage,
-  setTakeProfit,
-  createDeal,
-  updateTakeProfit,
-} = new OrderController();
+const { listCakes, createCake, getCakeDetails, updateCake, deleteCake } =
+  new CakeController();
+
 router.get('/', (req, res) => {
   res.json({ status: 'success', message: 'Welcome to Cake API Service' });
 });
 
-router.post('/orders', createOrder);
-router.get('/orders', getActiveOrder);
-router.get('/positions', getPosition);
-router.post('/leverage', setLeverage);
-router.post('/positions/tp', setTakeProfit);
-router.post('/deals', createDeal);
-router.get('/deals', updateTakeProfit);
+router.get('/cakes', listCakes);
+router.post('/cakes', Validator.validate(createCakeSchema), createCake);
+router.get('/cakes/:cake_id', getCakeDetails);
+router.put('/cakes/:cake_id', Validator.validate(createCakeSchema), updateCake);
+router.delete('/cakes/:cake_id', deleteCake);
+
 export default router;
